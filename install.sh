8185d0afb3dd337cff24fba3980ac0eb43b205be
ReecerTV <82712122+reecertv@users.noreply.github.com>
2024-05-23 18:23:35 +0200

    ...

--- install.sh
+++ install.sh
@@ -7,10 +7,16 @@ do
     read -rp "$DEST_DIR :: Is this correct? [Y/n] " input
 
     if [ "${input,,}" == "n" ]; then
-        exit 1
-    fi
+        echo "Next..."
+    else
+        cp "$file" ~/dotfiles/"$DEST_DIR"
 
-    cp "$file" ~/dotfiles/"$DEST_DIR"
+        #CONF_DIR=$(sed -n '2 s/.*\$\([^$]*\)\$.*/\1/p' "$file")
+
+        #sed -i "1s/.*/${HOME}/" file.txt
+    fi
 done
+
+# Set
 
 echo "Done!"
